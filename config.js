73b97e12fc8d66cfe45739e631b87db5632a98c9
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2022-01-30 11:45:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAfcqu9qDe7O1kbTJMZsmdJ8J1574Q7rM",
+    authDomain: "storytelling-app-6de9f.firebaseapp.com",
+    projectId: "storytelling-app-6de9f",
+    storageBucket: "storytelling-app-6de9f.appspot.com",
+    messagingSenderId: "917083539377",
+    appId: "1:917083539377:web:8f9821f623f1a152db5f90"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
